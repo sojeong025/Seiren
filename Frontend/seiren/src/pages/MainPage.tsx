c08d06cb8a9731ef9248066ac3fea1712871f1d9
protofu <sungjae0512@gmail.com>
2023-09-09 20:21:53 +0900

    Init : 230909 start pjt_seiren

---
+++ Frontend/seiren/src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      {/* 여기에 내용을 추가하세요 */}
+    </div>
+  );
+};
+
+export default MainPage;
