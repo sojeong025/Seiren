df11f7b77254f583825b77631a12297fef71494f
deerKBS <deer980119@gmail.com>
2023-09-11 10:45:42 +0900

    Init : 프로젝트 초기 설정 - h2 - jpa - 그 외 의존성 설정

---
+++ seiren/src/test/java/ssafy/e105/seiren/SeirenApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.e105.seiren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeirenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
