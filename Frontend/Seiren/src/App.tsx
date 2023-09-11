b89045ea1f2f28d0541a7ec1c9c48222b49a11c1
정소정 <thwjd0205@naver.com>
2023-09-11 14:52:43 +0900

    feat : react-router-dom and 라우터 설정

--- Frontend/Seiren/src/App.tsx
+++ Frontend/Seiren/src/App.tsx
@@ -1,12 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+// pages
+import MainPage from "./pages/MainPage";
+import AboutPage from "./pages/AboutPage";
+import VoiceMarketPage from "./pages/VoiceMarketPage";
+import VoiceStudyPage from "./pages/VoiceStudyPage";
+import MyPage from "./pages/MyPage";
+
+// common
+import NavBar from "./components/common/NavBar";
 
 function App() {
-  
   return (
-    <div>
-    <h1>Main</h1>
-    </div>
-  )
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/voice-market" element={<VoiceMarketPage />} />
+        <Route path="/voice-study" element={<VoiceStudyPage />} />
+        <Route path="/my-page" element={<MyPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
