ec43c606ffcf12cda840e676aa26cdacdda693a8
choiapple <79374648+choiapple@users.noreply.github.com>
2023-09-13 02:45:57 +0900

    Feat : kakao controller 수정

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/oauth/kakao/controller/kakaoController.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/oauth/kakao/controller/kakaoController.java
@@ -1,29 +1,33 @@
 package ssafy.e105.Seiren.domain.oauth.kakao.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 import ssafy.e105.Seiren.common.BaseResponse;
 import ssafy.e105.Seiren.domain.oauth.kakao.service.KakaoService;
 import ssafy.e105.Seiren.domain.user.dto.token.TokenDto;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/login/oauth2", produces = "application/json")
 @RequiredArgsConstructor
 @Tag(name = "OAUTH2.0 API")
-public class kakaocontroller {
+public class kakaoController {
 
     private final KakaoService kakaoService;
 
-    @GetMapping("/code/{registrationId}")
+
+    @Operation(summary = "kakaologin")
+    @GetMapping("/code/kakao")
     public BaseResponse kakaoOauth(HttpServletResponse response
             , @RequestParam("code") String code
-            , @PathVariable("registrationId") String registrationId
+//            , @PathVariable("registrationId") String registrationId
     ){
         try{
             TokenDto tokenDto = kakaoService.getKakaoInfo(code);
