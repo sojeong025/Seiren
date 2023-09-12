1b9bd556c15f49670c99403a0ca09cc9f828a5f3
protofu <sungjae0512@gmail.com>
2023-09-12 21:54:52 +0900

    Fix : MyPage[BuyList]

--- Frontend/Seiren/src/pages/MyPages/MyPage.tsx
+++ Frontend/Seiren/src/pages/MyPages/MyPage.tsx
@@ -1,7 +1,7 @@
 import styles from './MyPage.module.css';
-import MyInfo from '../components/MyProfile/MyInfo';
-import Likes from '../components/MyProfile/Likes';
-import YourVoice from '../components/MyProfile/YourVoice';
+import MyInfo from '../../components/MyProfile/MyInfo';
+import Likes from '../../components/MyProfile/Likes';
+import YourVoice from '../../components/MyProfile/YourVoice';
 
 function MyPage() {
   return (
