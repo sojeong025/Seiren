1d67166edbfc107e1c0fb47ca655992d1d2ddf24
jaeyoung <wodud9515@naver.com>
2023-09-13 20:57:22 +0900

    Feat : 상품 Entity 내용 수정

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/entity/Product.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/entity/Product.java
@@ -1,15 +1,18 @@
 package ssafy.e105.Seiren.domain.product.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ssafy.e105.Seiren.domain.voice.entity.Voice;
 
 @Data
 @Entity
@@ -22,7 +25,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
-    private Long voiceId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="voice_id")
+    private Voice voice;
     private String productTitle;
     private String summary;
     private Double price;
