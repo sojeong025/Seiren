86363fb4ec3417bf8cc2bffc57f059b2764c7bd5
deerKBS <deer980119@gmail.com>
2023-09-14 17:58:15 +0900

    Feat : Voice 도메인 에러 코드 정의

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/exception/VoiceErrorCode.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/exception/VoiceErrorCode.java
@@ -6,8 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum VoiceErrorCode {
-
-    SERVER_ERROR(500, "서버 에러");
+    SCRIPT_INSERT_ERROR(2030, "Script 추가 실패"),
+    NOT_EXIST_SCRIPT(2031, "존재하지 않는 Script"),
+    SCRIPT_DELETE_ERROR(2032, "Script 삭제 상태 변경 실패");
 
     private final int code;
     private final String message;
