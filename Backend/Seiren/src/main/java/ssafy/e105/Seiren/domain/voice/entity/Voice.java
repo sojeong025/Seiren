b36ce72ed24af63db815020c3a70d96472f4e1bf
deerKBS <deer980119@gmail.com>
2023-09-14 17:57:56 +0900

    fix : Voice entity class 수정

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/entity/Voice.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/entity/Voice.java
@@ -1,6 +1,5 @@
 package ssafy.e105.Seiren.domain.voice.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,14 +8,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
-import java.util.Date;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.ManyToAny;
 import ssafy.e105.Seiren.domain.user.entity.User;
 
 @Builder
