5ca1c622f9c60d9e0c36c8f76ab9aca801ab8cac
최진석 <cj5255@naver.com>
2023-09-15 16:06:15 +0900

    Feat : nickname중복체크 controller 수정 - pathparam에 수정

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/user/controller/UserController.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/user/controller/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 
     @Operation(summary = "닉네임 중복 체크")
     @GetMapping("/nicknames/chcek")
-    public ApiResult nicknameCheck(@RequestParam String nickname){
+    public ApiResult nicknameCheck(@RequestParam("nickname") String nickname){
         return ApiUtils.success(userService.nicknameCheck(nickname));
     }
 
