2098d23c6de96d9a3ad37f58a01f0e1a7b36dbd2
deerKBS <deer980119@gmail.com>
2023-09-15 17:28:31 +0900

    Feat : Voice 도메인 코드 추가

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/exception/VoiceErrorCode.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/exception/VoiceErrorCode.java
@@ -10,8 +10,9 @@ public enum VoiceErrorCode {
     NOT_EXIST_SCRIPT(3031, "존재하지 않는 Script"),
     SCRIPT_DELETE_ERROR(3032, "Script 삭제 상태 변경 실패"),
     FAIL_CREATE_VOICE(3033, "음성 생성 실패"),
-    NOT_EXSIT_VOICE(3034, "존재하지 않는 Voice")
-    ;
+    NOT_EXSIT_VOICE(3034, "존재하지 않는 Voice"),
+    UNMACHED_VOICE_USER(3035, "Voice 생성자와 사용자 id가 일치하지 않습니다."),
+    FAIL_UPDATE_VOICE(3036, "Voice 업데이트 실패");
 
     private final int code;
     private final String message;
--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/service/VoiceService.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/service/VoiceService.java
@@ -1,13 +1,18 @@
 package ssafy.e105.Seiren.domain.voice.service;
 
 import static ssafy.e105.Seiren.domain.voice.exception.VoiceErrorCode.FAIL_CREATE_VOICE;
+import static ssafy.e105.Seiren.domain.voice.exception.VoiceErrorCode.FAIL_UPDATE_VOICE;
 import static ssafy.e105.Seiren.domain.voice.exception.VoiceErrorCode.NOT_EXSIT_VOICE;
+import static ssafy.e105.Seiren.domain.voice.exception.VoiceErrorCode.UNMACHED_VOICE_USER;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ssafy.e105.Seiren.domain.user.entity.User;
 import ssafy.e105.Seiren.domain.user.service.UserService;
+import ssafy.e105.Seiren.domain.voice.dto.VoiceDto;
 import ssafy.e105.Seiren.domain.voice.dto.VoiceUpdateDto;
 import ssafy.e105.Seiren.domain.voice.entity.Voice;
 import ssafy.e105.Seiren.domain.voice.repository.VoiceRepository;
@@ -16,10 +21,15 @@ import ssafy.e105.Seiren.global.utils.ApiError;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class VoiceService {
 
     private final VoiceRepository voiceRepository;
     private final UserService userService;
+
+    public List<VoiceDto> getVoiceList(HttpServletRequest request) {
+        return voiceRepository.findVoiceJoin(userService.getUser(request).getId());
+    }
 
     public Long addVoice(HttpServletRequest request) {
         User user = userService.getUser(request);
@@ -32,14 +42,53 @@ public class VoiceService {
         }
     }
 
+    @Transactional
     public void updateVoice(HttpServletRequest request, VoiceUpdateDto voiceUpdateDto) {
         User user = userService.getUser(request);
         Voice voice = getVoice(voiceUpdateDto.getVoiceId());
         try {
-
+            if (voice.getUser() == user) {
+                voice.update(voiceUpdateDto);
+//                voiceRepository.save(voice);
+                return;
+            }
+            // voice 보유 유저 id와 업데이트 요청을 한 유저 id가 일치하지 않을 시 에러 처리
+            throw new BaseException(
+                    new ApiError(UNMACHED_VOICE_USER.getMessage(), UNMACHED_VOICE_USER.getCode()));
         } catch (Exception e) {
-            throw new BaseException(new ApiError("", 0));
+            throw new BaseException(
+                    new ApiError(FAIL_UPDATE_VOICE.getMessage(), FAIL_UPDATE_VOICE.getCode()));
         }
+    }
+
+    @Transactional
+    public void updateVoiceMemo(HttpServletRequest request, VoiceUpdateDto voiceUpdateDto) {
+        User user = userService.getUser(request);
+        Voice voice = getVoice(voiceUpdateDto.getVoiceId());
+        try {
+            if (voice.getUser() == user) {
+                voice.update(voiceUpdateDto.getMemo());
+                return;
+            }
+            // voice 보유 유저 id와 업데이트 요청을 한 유저 id가 일치하지 않을 시 에러 처리
+            throw new BaseException(
+                    new ApiError(UNMACHED_VOICE_USER.getMessage(), UNMACHED_VOICE_USER.getCode()));
+        } catch (Exception e) {
+            throw new BaseException(
+                    new ApiError(FAIL_UPDATE_VOICE.getMessage(), FAIL_UPDATE_VOICE.getCode()));
+        }
+    }
+
+    @Transactional
+    public void deleteVoice(HttpServletRequest request, Long voiceId) {
+        User user = userService.getUser(request);
+        Voice voice = getVoice(voiceId);
+        if (voice.getUser() == user) {
+            voice.delete();
+            return;
+        }
+        throw new BaseException(
+                new ApiError(UNMACHED_VOICE_USER.getMessage(), UNMACHED_VOICE_USER.getCode()));
     }
 
     public Voice getVoice(Long voiceId) {
