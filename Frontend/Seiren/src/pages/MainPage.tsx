31cf43f6af15ec468f5dd6134d9b6ba17fa9ea4f
protofu <sungjae0512@gmail.com>
2023-09-16 14:25:31 +0900

    Feat : mainPage test and navBar

--- Frontend/Seiren/src/pages/MainPage.tsx
+++ Frontend/Seiren/src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
 import styles from './MainPage.module.css'
 import danceduck from "../assets/duckdance.gif";
+import videoSource from "../assets/dance_-_95064 (1080p).mp4"; // 영상 파일 경로를 추가하세요.
 
 export default function MainPage() {
   return (
     <div>
-      <h1>main</h1>
+      {/* <h1>main</h1> */}
+      <video className={styles.video} autoPlay loop muted>
+        <source src={videoSource} type="video/mp4" />
+      </video>
       <img src={danceduck} className={styles.duck} alt="" />
     </div>
   );
