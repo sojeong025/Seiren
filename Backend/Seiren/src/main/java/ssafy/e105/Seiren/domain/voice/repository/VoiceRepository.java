36d4f632c2d06079ed98dbcdbfba2b1c9be39db7
deerKBS <deer980119@gmail.com>
2023-09-17 23:13:21 +0900

    Fix : 프로젝션 미적용으로 수정 - 프로젝션 처리 시 지속적인 에러 발생으로 인해 수정

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/repository/VoiceRepository.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/repository/VoiceRepository.java
@@ -3,13 +3,10 @@ package ssafy.e105.Seiren.domain.voice.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import ssafy.e105.Seiren.domain.voice.dto.VoiceDto;
 import ssafy.e105.Seiren.domain.voice.entity.Voice;
 
 public interface VoiceRepository extends JpaRepository<Voice, Long> {
 
-    @Query("select v from Voice v left join fetch Product")
-        // where v.userId = :userIdd
-    List<VoiceDto> findVoiceJoin(@Param("userId") Long userId);
+    @Query("select v from Voice v left join fetch v.product")
+    List<Voice> findByUser_Id(Long userId);
 }
