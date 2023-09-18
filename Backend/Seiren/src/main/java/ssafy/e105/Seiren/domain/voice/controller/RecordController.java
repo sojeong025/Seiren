3b464e8c35d086fec0a807fec470fcbc270773c8
deerKBS <deer980119@gmail.com>
2023-09-18 17:50:25 +0900

    Feat : 진행률 API 구현

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/controller/RecordController.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/controller/RecordController.java
@@ -3,6 +3,7 @@ package ssafy.e105.Seiren.domain.voice.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ssafy.e105.Seiren.domain.voice.service.RecordService;
 import ssafy.e105.Seiren.global.utils.ApiResult;
@@ -14,8 +15,14 @@ public class RecordController {
 
     private final RecordService recordService;
 
-    @GetMapping("/api/records/recent")
-    public ApiResult<?> getRecentRecord(HttpServletRequest request) {
-        return ApiUtils.success(recordService.getRecentRecordId(request));
+    @GetMapping("/api/records/recent/{voiceId}")
+    public ApiResult<?> getCurrentVoiceRecentScriptId(HttpServletRequest request,
+            @PathVariable Long voiceId) {
+        return ApiUtils.success(recordService.getRecentScriptId(request, voiceId));
+    }
+
+    @GetMapping("/api/records/count/{voiceId}")
+    public ApiResult<?> getRecordsCount(HttpServletRequest request, @PathVariable Long voiceId) {
+        return ApiUtils.success(recordService.getRecordsCount(request, voiceId));
     }
 }
