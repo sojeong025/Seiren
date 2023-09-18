c55f4357772c598339d54c7c4c110a62c0345e88
deerKBS <deer980119@gmail.com>
2023-09-19 03:31:12 +0900

    Feat : annotation 추가 및 toEntity 메서드 작성

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/entity/Record.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/entity/Record.java
@@ -8,27 +8,40 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Record {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long recordId;
 
-    @ManyToOne(fetch= FetchType.LAZY)
-    @JoinColumn(name="voice_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "voice_id")
     private Voice voice;
 
-    @ManyToOne(fetch= FetchType.LAZY)
-    @JoinColumn(name="script_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "script_id")
     private Script script;
 
     private String recordUrl;
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public static Record toEntity(Voice voice, Script script, String recordUrl) {
+        return Record.builder()
+                .voice(voice)
+                .script(script)
+                .recordUrl(recordUrl)
+                .build();
+    }
 }
