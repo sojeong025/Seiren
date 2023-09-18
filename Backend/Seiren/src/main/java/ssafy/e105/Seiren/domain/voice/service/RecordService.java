4b7ecc366d49808a1b5eca1db23c11681c6b5908
deerKBS <deer980119@gmail.com>
2023-09-18 17:56:26 +0900

    Feat : 진행률 API 구현을 위한 Service class 구현

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/service/RecordService.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/service/RecordService.java
@@ -5,19 +5,43 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssafy.e105.Seiren.domain.user.service.UserService;
+import ssafy.e105.Seiren.domain.voice.dto.RecordResponse;
 import ssafy.e105.Seiren.domain.voice.entity.Record;
 import ssafy.e105.Seiren.domain.voice.repository.RecordRepository;
+import ssafy.e105.Seiren.domain.voice.repository.ScriptRepository;
 
 @Service
 @RequiredArgsConstructor
 public class RecordService {
 
     private final RecordRepository recordRepository;
+    private final ScriptRepository scriptRepository;
     private final UserService userService;
 
-    public Long getRecentRecordId(HttpServletRequest request) {
-        Optional<Record> record = recordRepository.findTopByVoice_User_IdOrderByScript_IdDesc(
-                userService.getUser(request).getId());
-        return record.map(Record::getRecordId).orElse(-1L);
+    public Long getRecentScriptId(HttpServletRequest request, Long voiceId) {
+        Optional<Record> record = recordRepository.findTopByVoiceUserIdAndVoiceVoiceIdOrderByScriptScriptIdDesc(
+                userService.getUser(request).getId(), voiceId);
+
+        return record.map(r -> r.getScript().getScriptId()).orElse(0L);
     }
+
+    public Long getRecentScriptId(Long userId, Long voiceId) {
+        Optional<Record> record = recordRepository.findTopByVoiceUserIdAndVoiceVoiceIdOrderByScriptScriptIdDesc(
+                userId, voiceId);
+
+        return record.map(r -> r.getScript().getScriptId()).orElse(0L);
+    }
+
+    public RecordResponse getRecordsCount(HttpServletRequest request, Long voiceId) {
+        Long userId = userService.getUser(request).getId();
+        Integer recordCount = recordRepository.findCountByVoice_User_IdAndVoice_VoiceId(userId,
+                voiceId); // 내 녹음 횟수
+        Long scriptId = getRecentScriptId(userId, voiceId);
+        Integer totalCount =
+                scriptRepository.findCountByScriptIdGreaterThanAndIsDeleteFalse(scriptId)
+                        + recordCount;
+        return new RecordResponse(recordCount, totalCount);
+    }
+
+
 }
