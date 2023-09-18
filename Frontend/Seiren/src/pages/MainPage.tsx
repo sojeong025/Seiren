3a46b9d873c19c174c3a1cf32269a4d483ea04be
protofu <sungjae0512@gmail.com>
2023-09-18 11:35:54 +0900

    Feat : MainPage style

--- Frontend/Seiren/src/pages/MainPage.tsx
+++ Frontend/Seiren/src/pages/MainPage.tsx
@@ -4,19 +4,19 @@ import danceduck from '../assets/duckdance.gif';
 import videoSource from '../assets/night_-_28860 (1080p).mp4';
 
 export default function MainPage() {
-  const [textStyle, setTextStyle] = useState({
-    transform: 'perspective(4000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)',
-  });
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
 
   const handleMouseMove = (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
 
-    // 여기에서 원하는 움직임을 계산하세요.
-    // 예를 들어, 마우스 위치에 따라 텍스트를 움직이거나 변형합니다.
-    const transformValue = `perspective(4000px) rotateX(${mouseY}deg) rotateY(${mouseX}deg) scale3d(1, 1, 1)`;
-    
-    setTextStyle({ transform: transformValue });
+    const maxAngle = 30;
+    const angleX = (mouseY / window.innerHeight) * 2 * maxAngle - maxAngle;
+    const angleY = (mouseX / window.innerWidth) * 2 * maxAngle - maxAngle;
+
+    setRotateX(angleX);
+    setRotateY(angleY);
   };
 
   useEffect(() => {
@@ -26,24 +26,24 @@ export default function MainPage() {
     };
   }, []);
 
+  const textStyle = {
+    transform: `perspective(4000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1, 1, 1)`,
+  };
+
   return (
     <div className={styles.mainPageContainer}>
       <video className={styles.videoBackground} autoPlay loop muted>
         <source src={videoSource} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div style={textStyle}>
-        <div className={`w-[100vw] h-[100vh] relative`}>
-          <div className={`absolute left-[10vw] top-[5vw]`}>
-            <p className={`landing-text-shadow text-[calc(35px+1vw)] font-montserrat font-bold`}>
-              Voice of Web 3.0
-            </p>
-          </div>
-          <div className={`absolute bottom-[2vw] right-[calc(3vw+1vh)]`}>
-            <p className={`text-center landing-text-shadow text-[calc(4px+1.4vw)] font-montserrat font-semi-bold`}>
-              Full freedom to sound like <br /> anyone in the metaverse
-            </p>
-          </div>
+      <div style={textStyle} className={styles.textContainer}>
+        <div className={styles.text}>
+          <p className={styles.title}>
+            예혰예혰 움직인댱 ㅋㅋㅋ
+          </p>
+          <p className={styles.subtitle}>
+            Full freedom to sound like <br /> anyone in the metaverse
+          </p>
         </div>
       </div>
     </div>
