d5f304b48003467a82fcfa49d461f5bca50cd4e0
deerKBS <deer980119@gmail.com>
2023-09-19 03:43:50 +0900

    Feat : 생성자 추가

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/dto/VoiceUpdateDto.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/dto/VoiceUpdateDto.java
@@ -3,6 +3,7 @@ package ssafy.e105.Seiren.domain.voice.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ssafy.e105.Seiren.domain.voice.entity.Voice;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,4 +14,11 @@ public class VoiceUpdateDto {
     private String voiceTitle;
     private String memo;
     private String voiceAvatarUrl;
+
+    public VoiceUpdateDto(Voice voice) {
+        this.voiceId = voice.getVoiceId();
+        this.voiceTitle = voice.getVoiceTitle();
+        this.memo = voice.getMemo();
+        this.voiceAvatarUrl = voice.getVoiceAvatarUrl();
+    }
 }
