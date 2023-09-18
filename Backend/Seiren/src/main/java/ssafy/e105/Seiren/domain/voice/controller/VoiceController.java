d20bc3eacb79e3e3740e706e3d12254e22c37325
deerKBS <deer980119@gmail.com>
2023-09-19 03:39:11 +0900

    Feat : api 추가 - 음성 상세 정보 요청 API - AI 모델 생성 요청 API

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/controller/VoiceController.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/voice/controller/VoiceController.java
@@ -1,5 +1,6 @@
 package ssafy.e105.Seiren.domain.voice.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ssafy.e105.Seiren.domain.voice.dto.VoiceMemoUpdateRequest;
 import ssafy.e105.Seiren.domain.voice.dto.VoiceUpdateDto;
 import ssafy.e105.Seiren.domain.voice.service.VoiceService;
 import ssafy.e105.Seiren.global.utils.ApiResult;
@@ -16,24 +18,32 @@ import ssafy.e105.Seiren.global.utils.ApiUtils;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "음성 API")
 public class VoiceController {
 
     private final VoiceService voiceService;
 
-    @GetMapping("/api/progressingVoices/")
+    // 녹음 페이지 처음 접속 시 호출
+    @GetMapping("/api/progressingVoices")
     public ApiResult<?> getProgressVoiceId(HttpServletRequest request) {
         return ApiUtils.success(
-                voiceService.getCurrentVoiceId(request)); // 녹음 페이지 처음 접속 시 호출 - 현재 녹음 중인 음성 id
+                voiceService.getCurrentVoiceId(request));
     }
 
     @GetMapping("/api/voices")
-    public ApiResult<?> voiceList(HttpServletRequest request) {
+    public ApiResult<?> getVoiceList(HttpServletRequest request) {
         return ApiUtils.success(voiceService.getVoiceList(request));
+    }
+
+    @GetMapping("/api/voices/{voiceId}")
+    public ApiResult<?> getVoiceDetail(HttpServletRequest request, @PathVariable Long voiceId) {
+        return ApiUtils.success(voiceService.getVoiceDetail(request, voiceId));
     }
 
     @PostMapping("/api/voices")
     public ApiResult<?> addVoice(HttpServletRequest request) {
-        return ApiUtils.success(voiceService.addVoice(request)); // 현재 녹음중인 음성이 없으면 음성 생성 호출
+        return ApiUtils.success(
+                voiceService.addVoice(request)); // 현재 녹음중인 음성(0, 1)이 없으면 음성 생성 호출 필요
     }
 
     @PutMapping("/api/voices")
@@ -45,9 +55,15 @@ public class VoiceController {
 
     @PutMapping("/api/voices/memo")
     public ApiResult<?> modifyVoiceMemo(HttpServletRequest request,
-            @RequestBody VoiceUpdateDto voiceUpdateDto) {
-        voiceService.updateVoiceMemo(request, voiceUpdateDto);
+            @RequestBody VoiceMemoUpdateRequest voiceMemoUpdateRequest) {
+        voiceService.updateVoiceMemo(request, voiceMemoUpdateRequest);
         return ApiUtils.success("목소리 메모 수정 완료");
+    }
+
+    @PutMapping("/api/voices/AI")
+    public ApiResult<?> createAiModel(HttpServletRequest request) {
+
+        return ApiUtils.success("학습 요청 성공");
     }
 
     @DeleteMapping("/api/voices/{voiceId}")
