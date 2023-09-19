89f7273925fec3a5efbb78cd12176063f9f386a6
jaeyoung <wodud9515@naver.com>
2023-09-19 12:39:18 +0900

    Feat : 찜 목록 API 구현
    - 찜 목록 불러오기 기능 구현

---
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/dto/WishListDto.java
@@ -0,0 +1,11 @@
+package ssafy.e105.Seiren.domain.product.dto;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class WishListDto {
+
+    private List<ProductDto> wishList;
+
+}
--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/exception/ProductErrorCode.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/exception/ProductErrorCode.java
@@ -16,7 +16,8 @@ public enum ProductErrorCode {
     FAIL_UPDATE_PRODUCT(4006, "상품 업데이트 실패"),
     NOT_EXIST_WISH(4007, "존재하지 않는 Wish"),
     UNMACHED_WISH_USER(4008, "찜 등록자, 찜 취소 희망자 불일치"),
-    FAIL_DELETE_WISH(4009, "찜 취소 실패");
+    FAIL_DELETE_WISH(4009, "찜 취소 실패"),
+    FAIL_GET_WISHLIST(4010, "찜 록록 불러오기 실패");
     private final int code;
     private final String message;
 
