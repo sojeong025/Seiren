b95466d73c07b9bff27d11007b9cffe24036634a
protofu <sungjae0512@gmail.com>
2023-09-19 16:22:33 +0900

    Feat : VoiceUseItem Card

--- Frontend/Seiren/src/components/UseVoice/VoiceItem.tsx
+++ Frontend/Seiren/src/components/UseVoice/VoiceItem.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import styles from "./UseVoiceBox.module.css";
+import styles from "./VoiceItem.module.css";
 
-function VoiceItem({ profileImage, title, moodHashtag }) {
+function VoiceItem({ profileImage, title, moodHashtag, author }) {
   return (
     <div className={styles.voiceItem}>
-      <img src={profileImage} alt="프로필 사진" className={styles.profileImage} />
-      <div className={styles.title}>{title}</div>
-      <div className={styles.moodHashtag}>{moodHashtag}</div>
+      <div className={styles.imageContainer}>
+        <img src={profileImage} alt="프로필 사진" className={styles.profileImage} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.moodHashtag}>{moodHashtag}</div>
+        <div className={styles.authorDate}>
+          <span className={styles.author}>작성자: {author}</span>
+        </div>
+      </div>
       <button className={styles.playButton}>재생</button>
     </div>
   );
