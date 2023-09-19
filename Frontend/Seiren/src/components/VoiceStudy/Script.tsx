175c56e9f6348596aecc83d8b7ed0fdb678d09a8
정소정 <thwjd0205@naver.com>
2023-09-20 00:03:38 +0900

    Style : VoiceRecordPage 1차 Design

--- Frontend/Seiren/src/components/VoiceStudy/Script.tsx
+++ Frontend/Seiren/src/components/VoiceStudy/Script.tsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
 import styles from "./Script.module.css"
-import VoiceStudyHeader from "./VoiceStudyHeader";
 
-const Script: React.FC = () => {
+interface ScriptProps {
+  index: number; // 현재 script의 인덱스 값을 받는 props 추가
+  setIndex: React.Dispatch<React.SetStateAction<number>>; // index 변경 함수를 받는 props 추가
+}
+
+const Script: React.FC<ScriptProps> = ({ index, setIndex }) => {
   // 더미 데이터 생성
   const scripts = Array.from({length: 10}, (_, i) => `스크립트 문장 ${i+1}`);
-
-  const [index, setIndex] = useState<number>(0);
 
   const goNext = (): void => {
     if (index < scripts.length-1) setIndex(index+1)
   }
 
   return (
-    <div>
-      <VoiceStudyHeader currentIndex={index} /> {/* index 값을 prop으로 전달 */}
-      
+    <div>      
       <div className={styles.text}>
-        {scripts[index]}
-        <br />
-        {scripts[index + 1]}
-        <button onClick={goNext}>다음</button>
+        <div className={styles.text_now}>{scripts[index]}</div>
+        <div className={styles.text_next}>NEXT {scripts[index + 1]}</div>
+      </div>
+        
+      <hr className={styles.hr} />
+
+      <div className={styles.btn}>
+        <div className={styles.record2}>다시 녹음</div>
+        <div className={styles.next} onClick={goNext}>다음</div>
       </div>
     </div>
   );
 };
 
 export default Script;
-
--- Frontend/Seiren/src/components/VoiceStudy/VoiceStudyHeader.tsx
+++ Frontend/Seiren/src/components/VoiceStudy/VoiceStudyHeader.tsx
@@ -2,10 +2,11 @@ import { useNavigate } from 'react-router-dom';
 import styles from "./VoiceStudyHeader.module.css"
 
 interface VoiceStudyHeaderProps {
+  totalIndex: number; // 전체 script의 개수 받는 props 추가
   currentIndex: number; // 현재 script의 인덱스 값을 받는 props 추가
 }
 
-const VoiceStudyHeader: React.FC<VoiceStudyHeaderProps> = ({ currentIndex }) => {
+const VoiceStudyHeader: React.FC<VoiceStudyHeaderProps> = ({ totalIndex, currentIndex }) => {
   
   const isButtonDisabled = currentIndex < 9; // 버튼 활성화 여부 판단
   const navigate = useNavigate();
@@ -13,9 +14,13 @@ const VoiceStudyHeader: React.FC<VoiceStudyHeaderProps> = ({ currentIndex }) => 
     navigate('/voice-studying');
   }
 
+  const progress = (currentIndex / totalIndex) * 100;
+
   return (
     <div className={styles.header}>
-      <div>목소리 녹음</div>
+      <div className={styles.progressBarContainer}>
+        <div className={styles.progressBar} style={{width: `${progress}%`}}/>
+      </div>
       {/* isButtonDisabled 값에 따라 버튼의 disabled 속성 설정 */}
       <button className={styles.button_study} disabled={isButtonDisabled}
       onClick={handleButtonClick}>목소리 학습</button>
