a72f46bfbbf18bb970c0c7c1364b3e418dd7e34f
protofu <sungjae0512@gmail.com>
2023-09-21 14:38:21 +0900

    Feat : Side Bar

--- Frontend/Seiren/src/components/MyProfile/YourVoice.tsx
+++ Frontend/Seiren/src/components/MyProfile/YourVoice.tsx
@@ -2,30 +2,40 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function YourComponent() {
-  const [apiData, setApiData] = useState([]); // 초기값을 빈 배열로 설정
-  const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJudWxsIiwiaWF0IjoxNjk1MjYyMjMyLCJleHAiOjE2OTUyNjgyMzJ9.LC5rwDLYVZtF_rTor327rHBxbREIQe5I-RZKREf1XlA'; // 여기에 액세스 토큰을 넣어주세요
+  const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJudWxsIiwiaWF0IjoxNjk1MjczNDAxLCJleHAiOjE2OTUyNzk0MDF9.y5G8BFF4fo9jxS1Q41tB1yG8W_AkTFFsjfEUjySBYcY'; // Replace with your access token
+  const apiUrl = 'http://192.168.40.134:8080/api/voices';
 
   useEffect(() => {
-    const apiUrl = 'http://192.168.40.134:8080/api/voices'; // API 엔드포인트 URL
-
-    // API 호출 설정
     const config = {
       headers: {
-        Authorization: `Bearer ${accessToken}`, // 액세스 토큰 추가
+        Authorization: `Bearer ${accessToken}`,
       },
     };
 
-    // API 호출
     axios.get(apiUrl, config)
       .then((response) => {
-        // API 응답 데이터를 상태에 저장
-        setApiData(response.data.response); // response.data.response를 저장
+        setApiData(response.data.response);
+        setLoading(false); // Set loading to false when data is received
       })
       .catch((error) => {
-        // 오류 처리
-        console.error('API 호출 중 오류 발생:', error);
+        setError(error);
+        setLoading(false); // Set loading to false on error
       });
-  }, []);
+  }, [accessToken]);
+
+  // Render loading state while waiting for data
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render error message if there's an error
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div>
