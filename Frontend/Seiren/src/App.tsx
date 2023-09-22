c6f960a53c91c386221c131e8858636a0674b6b1
protofu <sungjae0512@gmail.com>
2023-09-22 09:42:07 +0900

    Feat : nav, side bar

--- Frontend/Seiren/src/App.tsx
+++ Frontend/Seiren/src/App.tsx
@@ -30,13 +30,13 @@ import OAuth2RedirectHandler from "./pages/LoginPages/OAuth2RedirectHandler";
 
 // 공통
 import NavBar from "./components/common/NavBar";
-// import SideBar from "./components/common/SideBar";
+import SideBar from "./components/common/SideBar";
 
 function App() {
   return (
     <Router>
-      {/* <SideBar /> */}
       <NavBar />
+      <SideBar />
         <Routes>
           <Route path="/" element={<MainPage />} />
         </Routes>
