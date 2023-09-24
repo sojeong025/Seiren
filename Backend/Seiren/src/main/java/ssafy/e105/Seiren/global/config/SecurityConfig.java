304ad011d9e7588f39f6f906a4ccddf5752a2799
deerKBS <deer980119@gmail.com>
2023-09-25 07:13:54 +0900

    Feat : 배포 설정 추가

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/global/config/SecurityConfig.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/global/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     private final CorsConfig corsConfig;
+
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -43,7 +44,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+            HandlerMappingIntrospector introspector) throws Exception {
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
         http.httpBasic().disable()
                 .csrf().disable()
@@ -51,21 +53,27 @@ public class SecurityConfig {
                 .and()
                 .apply(new MyCustomDsl())
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt token으로 인증 > 세션 필요없음
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt token으로 인증 > 세션 필요없음
 
         http.authorizeHttpRequests((req) -> req
-                                .requestMatchers(mvcMatcherBuilder.pattern("/api/login/oauth2/code/kakao")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern("/api/user/signup")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern("/api/user/login")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/h2-console/**")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "https://kauth.kakao.com/oauth/token")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "https://kapi.kakao.com/v2/user/me")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/favicon.ico")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/error")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/swagger-ui/**")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/swagger-resources/**")).permitAll()
-                                .requestMatchers(mvcMatcherBuilder.pattern( "/v3/api-docs/**")).permitAll()
-                                .anyRequest().authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/login/oauth2/code/kakao"))
+                        .permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/user/signup")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/user/login")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/h2-console/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("https://kauth.kakao.com/oauth/token"))
+                        .permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("https://kapi.kakao.com/v2/user/me"))
+                        .permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/favicon.ico")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/error")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/swagger-ui/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/swagger-resources/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/v3/api-docs/**")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/utils/profile")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/api/actuator/health")).permitAll()
+                        .anyRequest().authenticated()
                 )   // 다음 리퀘스트에 대한 사용권한 체크
                 .exceptionHandling()
                 .accessDeniedHandler(jwtAccessDeniedHandler)
@@ -78,9 +86,11 @@ public class SecurityConfig {
 
     // 커스텀 필터 추가를 여기서 처리하기
     public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            AuthenticationManager authenticationManager = http.getSharedObject(
+                    AuthenticationManager.class);
             http
                     // cors 오류를 해결하기 위해 Controller 에 @CrossOrigin 을 붙여주는 방법도 있지만
                     // 이 방식은 필터 추가와 다르게 인증이 필요 없는 url 만 처리해줌
