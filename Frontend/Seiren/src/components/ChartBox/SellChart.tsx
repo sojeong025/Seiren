5a874f4fe7432966d1e0aa589e832cd56d570e2d
protofu <sungjae0512@gmail.com>
2023-09-25 21:32:21 +0900

    Fix : useVoice

--- Frontend/Seiren/src/components/ChartBox/SellChart.tsx
+++ Frontend/Seiren/src/components/ChartBox/SellChart.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styles from "./SellChart.module.css";
+import { customAxios } from "../../libs/axios";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 function SellListBox() {
@@ -7,22 +8,19 @@ function SellListBox() {
   const [myProductLikes, setMyProductLikes] = useState(0);
   const [salesData, setSalesData] = useState([]); // 날짜와 판매량 데이터를 저장할 상태
 
-  // 더미 데이터를 사용하여 전체 판매수, 내 상품 찜 횟수, 그래프 데이터 설정
-  useEffect(() => {
-    // 전체 판매수와 내 상품 찜 횟수를 하드코딩된 값으로 설정
-    setTotalSales(1000); // 예시 값, 실제 데이터로 대체해야 합니다.
-    setMyProductLikes(50); // 예시 값, 실제 데이터로 대체해야 합니다.
+  // useEffect(() => {
+  //   customAxios
+  //     .get("Statistics/products") // 원하는 API 경로로 변경하세요.
+  //     .then(response => {
+  //       const responseData = response.data;
+  //       console.log(responseData)
 
-    // 더미 그래프 데이터 생성 (날짜와 판매량)
-    const dummySalesData = [
-      { date: "2023-09-01", sales: 100 },
-      { date: "2023-09-02", sales: 150 },
-      { date: "2023-09-03", sales: 200 },
-      // 더 많은 데이터를 추가할 수 있습니다.
-    ];
 
-    setSalesData(dummySalesData);
-  }, []);
+  //     })
+  //     .catch(error => {
+  //       console.error("API 호출 중 오류 발생:", error);
+  //     });
+  // }, []);
 
   return (
     <div className={styles.allContainer}>
