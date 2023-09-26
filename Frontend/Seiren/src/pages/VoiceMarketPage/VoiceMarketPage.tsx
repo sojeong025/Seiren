d0efd15f087974dd14aefbf92ea9801549d7398e
정소정 <thwjd0205@naver.com>
2023-09-26 15:40:35 +0900

    Feat: 상품 전체 조회 및 필터별 post 요청 필요

--- Frontend/Seiren/src/pages/VoiceMarketPage/VoiceMarketPage.tsx
+++ Frontend/Seiren/src/pages/VoiceMarketPage/VoiceMarketPage.tsx
@@ -1,100 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import FavoriteVoice from "../../components/VoiceMarket/FavoriteVoice";
 import Filter from "../../components/VoiceMarket/Filter";
 import styles from "./VoiceMarketPage.module.css"
 import { Link } from 'react-router-dom';
+import { customAxios } from "../../libs/axios";
 
-
-interface Voice {
+interface Product {
   id: number;
   avatar: string;
   title: string;
   seller: string;
-  category: string;
+  category: string[];
 }
 
-const voices: Voice[] = [
-  {
-    id: 1,
-    avatar: "avatar_url_1",
-    title: "Voice Title 1",
-    seller: "Seller Nickname 1",
-    category: "Category 1"
-  },
-  {
-    id: 2,
-    avatar: "avatar_url_2",
-    title: "Voice Title 2",
-    seller: "Seller Nickname 2",
-    category: "Category 2"
-  },
-  {
-    id: 1,
-    avatar: "avatar_url_1",
-    title: "Voice Title 1",
-    seller: "Seller Nickname 1",
-    category: "Category 1"
-  },
-  {
-    id: 3,
-    avatar: "avatar_url_2",
-    title: "Voice Title 2",
-    seller: "Seller Nickname 2",
-    category: "Category 2"
-  },
-  {
-    id: 4,
-    avatar: "avatar_url_1",
-    title: "Voice Title 1",
-    seller: "Seller Nickname 1",
-    category: "Category 1"
-  },
-  {
-    id: 5,
-    avatar: "avatar_url_2",
-    title: "Voice Title 2",
-    seller: "Seller Nickname 2",
-    category: "Category 2"
-  },
-  {
-    id: 6,
-    avatar: "avatar_url_1",
-    title: "Voice Title 1",
-    seller: "Seller Nickname 1",
-    category: "Category 1"
-  },
-  {
-    id: 7,
-    avatar: "avatar_url_2",
-    title: "Voice Title 2",
-    seller: "Seller Nickname 2",
-    category: "Category 2"
-  },
-
-];
-function VoiceCard({ voice }: { voice : Voice }) {
+function ProductCard({ product }: { product : Product }) {
   return (
-    <Link to={`/voice/${voice.id}`} className={styles.card}>
-      <img src={voice.avatar} alt="Avatar" />
-      <h3>{voice.title}</h3>
-      <p>{voice.seller}</p>
-      <p>{voice.category}</p>
+    <Link to={`/product/${product.id}`} className={styles.card}>
+      <img src={product.avatar} alt="Avatar" width={"100px"} />
+      <h3>{product.title}</h3>
+      <p>{product.seller}</p>
+      {product.category.map((category) => (
+        <p key={category}>{category}</p>
+      ))}
     </Link>
   );
 }
+
 function VoiceMarketPage() {
-  return (
-    <div>
-      <FavoriteVoice/>
-      <Filter />
-      <div className={styles.container}>
-        <div className={styles.cards}>
-          {voices.map((voice, index) => (
-            <VoiceCard key={index} voice={voice} />
-          ))}
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await customAxios.post('/products', {
+        "nickname": "재키",
+        "categoryIdList": [6,8],
+        "sortType": "Latest"
+      });
+
+      
+      if(response.data.success) {
+        const productsData = response.data.response.productDtoList.map((product:any) => ({
+          id: product.productId,
+          avatar: product.productImageUrl,
+          title: product.title,
+          seller:"",
+          category: product.productCategoryList.map((category:any)=>category.categoryName)
+        }));
+        setProducts(productsData);
+      }
+    };
+    fetchProducts();
+  }, []); 
+
+return (
+  <div>
+    <FavoriteVoice/>
+    <Filter />
+    <div className={styles.container}>
+      <div className={styles.cards}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </div>
-    </div>
-  );
+  </div>
+);
+
 }
 
 export default VoiceMarketPage;
