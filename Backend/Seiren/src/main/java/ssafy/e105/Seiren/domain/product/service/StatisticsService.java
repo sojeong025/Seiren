d9f3996c537e50962583cb16fced489159f44eea
jaeyoung <wodud9515@naver.com>
2023-09-26 10:19:17 +0900

    Feat : 상품_통계 리스트 상세, 전체 통계_한 달 단위 일자 별 수익 리스트 API 구현

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/service/StatisticsService.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/service/StatisticsService.java
@@ -3,20 +3,26 @@ package ssafy.e105.Seiren.domain.product.service;
 import static ssafy.e105.Seiren.domain.user.exception.UserErrorCode.NOT_EXIST_USER;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ssafy.e105.Seiren.domain.product.dto.ProductStatisticsDetailDto;
 import ssafy.e105.Seiren.domain.product.dto.ProductStatisticsDto;
 import ssafy.e105.Seiren.domain.product.entity.Product;
 import ssafy.e105.Seiren.domain.product.entity.Wish;
 import ssafy.e105.Seiren.domain.product.repository.ProductRepository;
 import ssafy.e105.Seiren.domain.product.repository.WishRepository;
+import ssafy.e105.Seiren.domain.transaction.entity.TransactionDescription;
+import ssafy.e105.Seiren.domain.transaction.repository.TransactionDescriptionRepository;
 import ssafy.e105.Seiren.domain.transaction.repository.TransactionRepository;
 import ssafy.e105.Seiren.domain.user.entity.User;
 import ssafy.e105.Seiren.domain.user.repository.UserRepository;
 import ssafy.e105.Seiren.domain.voice.entity.Voice;
-import ssafy.e105.Seiren.domain.voice.repository.VoiceRepository;
 import ssafy.e105.Seiren.global.error.type.BaseException;
 import ssafy.e105.Seiren.global.jwt.JwtTokenProvider;
 import ssafy.e105.Seiren.global.utils.ApiError;
@@ -30,6 +36,7 @@ public class StatisticsService {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final TransactionDescriptionRepository transactionDescriptionRepository;
 
 
     public int countWish(HttpServletRequest request) {
@@ -60,13 +67,53 @@ public class StatisticsService {
             productStatisticsDtoList.add(productStatisticsDto);
         }
         return productStatisticsDtoList;
+    }
 
-//        Page<ProductStatisticsDto> productStatisticsDtoList = transactionRepository.findAllBySeller(
-//                user.getId(), pageable);
-//        List<ProductStatisticsDto> productStatisticsDtoList = new ArrayList<>();
-//        for (Transaction transaction : transactionPage) {
-//            ProductStatisticsDto productStatisticsDto = new ProductStatisticsDto(transaction);
-//        }
+    public List<ProductStatisticsDetailDto> getStatisticsDetail(HttpServletRequest request,
+            Long productId) {
+        User user = getUser(request);
+        List<ProductStatisticsDetailDto> productStatisticsDetailDtoList = new ArrayList<>();
+        List<TransactionDescription> transactionDescriptionList = transactionDescriptionRepository.findByProductIdAndSellerIdOrderByBuyDateDESC(
+                productId, user.getId());
+        System.out.println("size : " + transactionDescriptionList.size());
+        for (TransactionDescription transactionDescription : transactionDescriptionList) {
+            ProductStatisticsDetailDto productStatisticsDetailDto = new ProductStatisticsDetailDto(
+                    transactionDescription);
+            productStatisticsDetailDtoList.add(productStatisticsDetailDto);
+        }
+        return productStatisticsDetailDtoList;
+    }
+
+    public Map<LocalDate, Double> getStatistics(HttpServletRequest request, int month) {
+        User user = getUser(request);
+
+        List<TransactionDescription> transactionDescriptionList = transactionDescriptionRepository.findBySellerIdAndMonth(
+                user.getId(), month);
+        return calculateTotalPriceByDate(transactionDescriptionList);
+    }
+
+    public Map<LocalDate, Double> calculateTotalPriceByDate(
+            List<TransactionDescription> transactionDescriptions) {
+        // LocalDateTime을 LocalDate로 변환하고 일자(day) 별로 그룹화하여 수익을 계산합니다.
+        Map<LocalDate, Double> totalPriceByDate = transactionDescriptions.stream()
+                .collect(Collectors.groupingBy(
+                        td -> td.getBuyDate().toLocalDate(),
+                        Collectors.summingDouble(td -> td.getPrice() * td.getBuyLetterCount())
+                ));
+
+        return totalPriceByDate;
+    }
+
+    public Map<YearMonth, Double> calculateMonthlyRevenue(
+            Map<LocalDate, Double> monthlyStatistics) {
+        // 월별로 그룹화하여 월별 수익을 계산합니다.
+        Map<YearMonth, Double> monthlyRevenue = monthlyStatistics.entrySet().stream()
+                .collect(Collectors.groupingBy(
+                        entry -> YearMonth.from(entry.getKey()),
+                        Collectors.summingDouble(Map.Entry::getValue)
+                ));
+
+        return monthlyRevenue;
     }
 
     public User getUser(HttpServletRequest request) {
