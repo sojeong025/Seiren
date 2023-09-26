5fa6a714542de2a2335f44ab981e12076037d253
정소정 <thwjd0205@naver.com>
2023-09-26 11:04:27 +0900

    Style : Voicemarket filter and search

--- Frontend/Seiren/src/components/VoiceMarket/Filter.tsx
+++ Frontend/Seiren/src/components/VoiceMarket/Filter.tsx
@@ -7,13 +7,18 @@ import styles from "./Filter.module.css"
 
 function Filter() {
   const [gender, setGender] = useState<string | "">('');
+  const [age, setAge] = useState<string | "">('');
+  const [mood, setMood] = useState<string | "">('');
 
   const handleChange = (event: SelectChangeEvent) => {
     setGender(event.target.value === undefined ? '' : event.target.value);
+    setAge(event.target.value === undefined ? '' : event.target.value);
+    setMood(event.target.value === undefined ? '' : event.target.value);
   };
 
   return (
     <div className={styles.filter}>
+      {/* 필터 넣는 곳 */}
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
         <InputLabel id="demo-simple-select-standard-label">Gender</InputLabel>
         <Select
@@ -30,6 +35,44 @@ function Filter() {
           <MenuItem value={'Female'}>Female</MenuItem>
         </Select>
       </FormControl>
+
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={age}
+          onChange={handleChange}
+          label="age"
+        >
+          <MenuItem value="">
+            <em>All</em>
+          </MenuItem>
+          <MenuItem value={'Children'}>Children</MenuItem>
+          <MenuItem value={'Female'}></MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Mood</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={mood}
+          onChange={handleChange}
+          label="mood"
+        >
+          <MenuItem value="">
+            <em>All</em>
+          </MenuItem>
+          <MenuItem value={'Male'}>Male</MenuItem>
+          <MenuItem value={'Female'}>Female</MenuItem>
+        </Select>
+      </FormControl>
+      <div>
+        <input className={styles.searchbar} placeholder='seach user nickname' type="text" />
+        <button type="submit" className={styles.search_btn}>검색</button>
+      </div>
     </div>
   );
 }
