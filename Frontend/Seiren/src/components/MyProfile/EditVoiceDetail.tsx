ea028f4a3ab27dce73caa4119f88155611df15e0
protofu <sungjae0512@gmail.com>
2023-09-26 11:34:54 +0900

    Fix : your voice detail

--- Frontend/Seiren/src/components/MyProfile/EditVoiceDetail.tsx
+++ Frontend/Seiren/src/components/MyProfile/EditVoiceDetail.tsx
@@ -5,7 +5,7 @@ import styles from "./YourVoiceDetail.module.css";
 
 function EditVoiceDetail() {
   const { voiceId } = useParams();
-  const [voiceDetail, setVoiceDetail] = useState(null); // 초기 값을 null로 설정
+  const [voiceDetail, setVoiceDetail] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const voiceTitleRef = useRef(null);
   const memoRef = useRef(null);
@@ -13,11 +13,11 @@ function EditVoiceDetail() {
   useEffect(() => {
     customAxios
       .get(`voices/${voiceId}`)
-      .then(response => {
+      .then((response) => {
         const voiceDetailData = response.data.response;
         setVoiceDetail(voiceDetailData);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("API 호출 중 오류 발생:", error);
       });
   }, [voiceId]);
@@ -28,49 +28,44 @@ function EditVoiceDetail() {
 
   const handleSaveClick = () => {
     const updatedData = {
-      voiceId: voiceDetail.voiceId,
       voiceTitle: voiceTitleRef.current.value,
       memo: memoRef.current.value,
-      voiceAvatarUrl: voiceDetail.voiceAvatarUrl,
     };
-  
+
     customAxios
-      .put(`voices`, updatedData)
-      .then(response => {
+      .put(`voices/${voiceId}`, updatedData)
+      .then((response) => {
+        console.log("수정 완료:", response.data);
+        setVoiceDetail(response.data);
         setIsEditing(false);
-        // 수정된 데이터로 상태 업데이트
-        setVoiceDetail(prevState => ({
-          ...prevState,
-          voiceTitle: updatedData.voiceTitle,
-          memo: updatedData.memo,
-        }));
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("수정 중 오류 발생:", error);
-        // 오류 처리 추가 가능
       });
   };
-  
 
-  if (voiceDetail === null) {
-    // 데이터 로딩 중인 경우 로딩 상태 표시
+  if (!voiceDetail) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className={styles.YourVoiceDetailContainer}>
       {isEditing ? (
-        <div>
-          <input type="text" ref={voiceTitleRef} defaultValue={voiceDetail.voiceTitle} />
+        <>
+          <input
+            type="text"
+            ref={voiceTitleRef}
+            defaultValue={voiceDetail.voiceTitle}
+          />
           <textarea ref={memoRef} defaultValue={voiceDetail.memo} />
           <button onClick={handleSaveClick}>저장</button>
-        </div>
+        </>
       ) : (
-        <div>
-          <div>{voiceDetail.voiceTitle}</div>
-          <div>Memo: {voiceDetail.memo}</div>
+        <>
+          <h1>{voiceDetail.voiceTitle}</h1>
+          <p>Memo: {voiceDetail.memo}</p>
           <button onClick={handleEditClick}>수정</button>
-        </div>
+        </>
       )}
       <img src={voiceDetail.voiceAvatarUrl} alt={voiceDetail.voiceTitle} />
     </div>
