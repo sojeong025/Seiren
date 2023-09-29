749bab2eed555fb2944e707588aa05669aac5c04
정소정 <thwjd0205@naver.com>
2023-09-30 05:37:27 +0900

    Style: 목소리 학습 중 페이지

--- Frontend/Seiren/src/pages/VoiceStudyPages/VoiceStudyingPage.tsx
+++ Frontend/Seiren/src/pages/VoiceStudyPages/VoiceStudyingPage.tsx
@@ -3,8 +3,23 @@ import styles from './VoiceStudyingPage.module.css'
 function VoiceStudyingPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.text}>
-        목소리 학습 완료시까지 3일정도의 시간이 소요됩니다
+      <div className={styles.contain}>
+        <div>
+          <img className={styles.img} src="src/assets/img/working.png" alt="학습중" />
+        </div>
+        {/* <div className={styles.percent}>
+          20%
+        </div> */}
+        <div className={styles.txt}>
+          <div className={styles.text}>
+            목소리를 생성하고 있어요
+          </div>
+          <hr />
+          <div className={styles.sidetext}>
+            완료 후 자동으로 페이지가 전환돼요. <br/>
+            학습을 완료하는 데는 대략 1시간이 소요됩니다.
+          </div>
+        </div>
       </div>
     </div>
   )
