7eba3b711cd502e343f3a3758d7a305b3996474a
protofu <sungjae0512@gmail.com>
2023-10-04 00:45:35 +0900

    Fix : fix

--- Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
+++ Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
@@ -1,54 +1,54 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { AuthenticationService } from './AuthenticationService';
 
-// OAuth2 인증 처리를 위한 컴포넌트
 const OAuth2RedirectHandler: React.FC = () => {
-  // URL에서 파라미터 추출
-  alert('OAuth2');
-  let params = new URL(document.URL).searchParams;
-  console.log(params);
-  let code = params.get('code');
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   const [check, setCheck] = useState(false);
+
+  // URL에서 'code' 파라미터 추출
+  const searchParams = new URLSearchParams(window.location.search);
+  const code = searchParams.get('code');
 
   // 토큰 요청 및 저장하는 함수
   async function fetchToken() {
     try {
-        const res = await AuthenticationService.kakaoLogin(code as string);
+      if (code) {
+        const res = await AuthenticationService.kakaoLogin(code);
         console.log('kakaoLogin 성공');
-        console.log(res);
+        console.log(res.data);
 
         // 토큰을 로컬 스토리지에 저장
-        localStorage.setItem('kakaoLogin', "true");
+        localStorage.setItem('accessToken', res.data.response.accessToken);
 
         // JWT 로그인 처리
         AuthenticationService.registerSuccessfulLoginForJwt(res.data.response.accessToken);
-      } catch (error) {
-        console.log('kakaoLogin 실패');
+      } else {
+        console.log('code 파라미터가 없습니다.');
       }
+    } catch (error) {
+      console.error('kakaoLogin 실패', error);
     }
-    
+  }
 
-    // 컴포넌트가 마운트될 때 한 번만 실행
-    useEffect(()=>{
-      setCheck(true);
-    },[]);
-    
-    // check 상태 변경 시 실행되는 효과
-    useEffect(() => {
-      if(check === true){
-        fetchToken(); // 토큰 요청 및 저장
-        navigate('/');
-      }
-    }, [check]);
-    
-    
+  // 컴포넌트가 마운트될 때 한 번만 실행
+  useEffect(() => {
+    setCheck(true);
+  }, []);
+
+  // check 상태 변경 시 실행되는 효과
+  useEffect(() => {
+    if (check === true) {
+      fetchToken(); // 토큰 요청 및 저장
+      navigate('/');
+    }
+  }, [check, navigate]);
+
   return (
     <div>
       로그인 중
     </div>
-  )
+  );
 }
 
 export default OAuth2RedirectHandler;
