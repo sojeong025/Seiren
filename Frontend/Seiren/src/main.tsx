e54d5adfe6c631c87fba51baad74518baa394986
protofu <sungjae0512@gmail.com>
2023-10-04 02:58:23 +0900

    Fix : di

--- Frontend/Seiren/src/main.tsx
+++ Frontend/Seiren/src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RecoilRoot } from 'recoil'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import './index.css';
 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
