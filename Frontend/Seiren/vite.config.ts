f2f1d54a6b18d27b270d1bfa5004ae0ab2663b00
protofu <sungjae0512@gmail.com>
2023-10-03 20:59:43 +0900

    Fix :fix

--- Frontend/Seiren/vite.config.ts
+++ Frontend/Seiren/vite.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   build: {
-    minify: false
+    minify: 'terser', // Terser를 사용하여 코드를 최소화합니다.
+    rollupOptions: {
+      // 기타 Rollup 옵션을 설정합니다.
+      // 필요한 경우 코드 스플리팅 등을 활성화할 수 있습니다.
+    },
+  },
+  optimizeDeps: {
+    // optimizeDeps를 비활성화하지 않고 사용합니다.
   },
   define: {
     'global' : {},
