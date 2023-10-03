c6fbb567361cf7003967b633ab45b25a233873b5
protofu <sungjae0512@gmail.com>
2023-10-04 01:41:25 +0900

    Fix : di

--- Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
+++ Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
@@ -1,52 +1,51 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import { AuthenticationService } from './AuthenticationService';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios, { AxiosResponse } from 'axios';
 
-// OAuth2 인증 처리를 위한 컴포넌트
+// 로그인 처리를 위한 컴포넌트
 const OAuth2RedirectHandler: React.FC = () => {
-  // URL에서 파라미터 추출
-  let params = new URL(document.URL).searchParams;
-  let code = params.get('code');
-  let navigate = useNavigate();
-  const [check, setCheck] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
-  // 토큰 요청 및 저장하는 함수
-  async function fetchToken() {
-    try {
-        const res = await AuthenticationService.kakaoLogin(code as string);
+  useEffect(() => {
+    // URL에서 파라미터 추출
+    const params = new URL(window.location.href).searchParams;
+    const code = params.get('code');
+
+    // 토큰 요청 및 저장
+    async function fetchToken() {
+      try {
+        const response = await axios.get(`/api/login/oauth2/code/kakao?code=${code}`);
         console.log('kakaoLogin 성공');
-        console.log(res);
+        console.log(response.data);
 
         // 토큰을 로컬 스토리지에 저장
-        localStorage.setItem('kakaoLogin', "true");
+        localStorage.setItem('accessToken', response.data.accessToken);
 
-        // JWT 로그인 처리
-        AuthenticationService.registerSuccessfulLoginForJwt(res.data.response.accessToken);
+        // JWT 로그인 처리 또는 다른 로직을 수행
+        // AuthenticationService.registerSuccessfulLoginForJwt(response.data.accessToken);
+
+        // 로그인이 완료되면 홈 페이지로 이동
+        navigate('/');
       } catch (error) {
-        console.log('kakaoLogin 실패');
+        console.error('kakaoLogin 실패', error);
+      } finally {
+        setIsLoading(false);
       }
     }
-    
 
-    // 컴포넌트가 마운트될 때 한 번만 실행
-    useEffect(()=>{
-      setCheck(true);
-    },[]);
-    
-    // check 상태 변경 시 실행되는 효과
-    useEffect(() => {
-      if(check === true){
-        fetchToken(); // 토큰 요청 및 저장
-        navigate('/');
-      }
-    }, [check]);
-    
-    
-  return (
-    <div>
-      로그인 중
-    </div>
-  )
-}
+    if (code) {
+      fetchToken();
+    } else {
+      setIsLoading(false);
+    }
+  }, [navigate]);
+
+  if (isLoading) {
+    return <div>로그인 중...</div>;
+  }
+
+  return <div>로그인 완료!</div>;
+};
 
 export default OAuth2RedirectHandler;
