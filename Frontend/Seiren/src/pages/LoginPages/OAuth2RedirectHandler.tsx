20f3e9833b454ad568adf711bed2562c8dcbe79c
protofu <sungjae0512@gmail.com>
2023-10-04 02:30:31 +0900

    Fix : di

--- Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
+++ Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
@@ -2,29 +2,27 @@ import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { AuthenticationService } from './AuthenticationService';
 
-// OAuth2 인증 처리를 위한 컴포넌트
 const OAuth2RedirectHandler: React.FC = () => {
-  // URL에서 파라미터 추출
   let params = new URL(document.URL).searchParams;
   let code = params.get('code');
   let navigate = useNavigate();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const handleLogin = async () => {
       try {
         const response = await AuthenticationService.kakaoLogin(code);
         console.log('kakao로그인 성공');
         console.log(response);
 
         AuthenticationService.registerSuccessfulLoginForJwt(response.data.response.accessToken);
-        navigate('/');
       } catch (err) {
-        console.log('카카오 로그인 실패', err);
-        // 오류 처리 로직을 추가할 수 있습니다.
+        console.log('카카오 로그인 실패')
       }
+
+      navigate('/');
     };
 
-    fetchData();
+    handleLogin();
   }, [code, navigate]);
     
   return (
