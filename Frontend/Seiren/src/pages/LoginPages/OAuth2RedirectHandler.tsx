0eca4a3fe0069cf0bd07267b1ceceae74ea92b73
protofu <sungjae0512@gmail.com>
2023-10-04 02:00:24 +0900

    Fix : di

--- Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
+++ Frontend/Seiren/src/pages/LoginPages/OAuth2RedirectHandler.tsx
@@ -1,33 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 
-// 로그인 처리를 위한 컴포넌트
 const OAuth2RedirectHandler: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
-  console.log('3');
+
   useEffect(() => {
     // URL에서 파라미터 추출
     const params = new URL(window.location.href).searchParams;
     const code = params.get('code');
-    console.log('4');
+
     // 토큰 요청 및 저장
     async function fetchToken() {
-      console.log('5');
       try {
-        const response = await axios.get(`/api/login/oauth2/code/kakao?code=${code}`);
-        console.log('kakaoLogin 성공');
-        console.log(response.data);
+        if (code) {
+          const response = await axios.get(`/api/login/oauth2/code/kakao?code=${code}`);
+          console.log('kakaoLogin 성공');
+          console.log(response.data);
 
-        // 토큰을 로컬 스토리지에 저장
-        localStorage.setItem('accessToken', response.data.accessToken);
+          // 토큰을 로컬 스토리지에 저장
+          localStorage.setItem('accessToken', response.data.accessToken);
 
-        // JWT 로그인 처리 또는 다른 로직을 수행
-        // AuthenticationService.registerSuccessfulLoginForJwt(response.data.accessToken);
-
-        // 로그인이 완료되면 홈 페이지로 이동
-        navigate('/');
+          // JWT 로그인 처리 또는 다른 로직을 수행
+          // AuthenticationService.registerSuccessfulLoginForJwt(response.data.accessToken);
+        } else {
+          console.error('인증 코드가 없습니다.');
+        }
       } catch (error) {
         console.error('kakaoLogin 실패', error);
       } finally {
@@ -35,11 +34,7 @@ const OAuth2RedirectHandler: React.FC = () => {
       }
     }
 
-    if (code) {
-      fetchToken();
-    } else {
-      setIsLoading(false);
-    }
+    fetchToken();
   }, [navigate]);
 
   if (isLoading) {
