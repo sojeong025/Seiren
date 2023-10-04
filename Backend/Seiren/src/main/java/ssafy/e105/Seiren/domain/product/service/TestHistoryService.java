49a2db72f1b6ebfeb70a467947f0ad681b211982
deerKBS <deer980119@gmail.com>
2023-10-04 15:58:00 +0900

    Fix : 에러 확인을 위해 로그 추가

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/service/TestHistoryService.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/product/service/TestHistoryService.java
@@ -6,11 +6,11 @@ import static ssafy.e105.Seiren.domain.user.exception.UserErrorCode.NOT_EXIST_US
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ssafy.e105.Seiren.domain.product.entity.Product;
 import ssafy.e105.Seiren.domain.product.entity.TestHistory;
-import ssafy.e105.Seiren.domain.product.entity.Wish;
 import ssafy.e105.Seiren.domain.product.repository.TestHistoryRepository;
 import ssafy.e105.Seiren.domain.user.entity.User;
 import ssafy.e105.Seiren.domain.user.repository.UserRepository;
@@ -21,6 +21,7 @@ import ssafy.e105.Seiren.global.utils.ApiError;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class TestHistoryService {
 
     private final TestHistoryRepository testHistoryRepository;
@@ -31,11 +32,14 @@ public class TestHistoryService {
     @Transactional
     public Integer checkTestCount(Long productId, HttpServletRequest request) {
         User user = getUser(request);
+        log.debug(user.getId() + ", prodictId : " + productId);
         if (countTest(productId, request) > 0) {
             TestHistory testHistory = getTestHistory(user.getId(), productId);
             testHistory.update();
-            return  testHistory.getCount();
+            log.debug(String.valueOf(testHistory.getCount()));
+            return testHistory.getCount();
         }
+        log.debug(user.getId() + ", prodictId : " + productId + ": 횟수 초과!");
         throw new BaseException(
                 new ApiError(OVER_RESTCOUNT.getMessage(), OVER_RESTCOUNT.getCode()));
     }
