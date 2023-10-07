010dc41828026f8c5aea56620890c858429a7007
protofu <sungjae0512@gmail.com>
2023-10-07 21:15:59 +0900

    Fix : all

--- Frontend/Seiren/src/libs/axios.tsx
+++ Frontend/Seiren/src/libs/axios.tsx
@@ -10,14 +10,14 @@ export const customAxios = axios.create({
 });
 
 customAxios.interceptors.request.use(
-  (config) => {
+  config => {
     const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
   },
 );
