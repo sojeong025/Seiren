9ccb2eb0672420855462df29f04261aef59fe475
choiapple <79374648+choiapple@users.noreply.github.com>
2023-10-07 18:49:06 +0900

    Feat : 로그인 관련 로직 수정
    - 닉네임 받아오는거로 바꿈
    - 이메일을 아이디로 바꿈

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/oauth/kakao/service/KakaoService.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/oauth/kakao/service/KakaoService.java
@@ -114,16 +114,16 @@ public class KakaoService {
         JSONObject account = (JSONObject) jsonObj.get("kakao_account");
         JSONObject profile = (JSONObject) account.get("profile");
 
-        long id = (long) jsonObj.get("id");
+        String id = String.valueOf(jsonObj.get("id"));
         String email = String.valueOf(account.get("email"));
-//        String nickname = String.valueOf(profile.get("nickname"));
+        String nickname = String.valueOf(profile.get("nickname"));
         String profileImg = String.valueOf(profile.get("profile_image_url"));
 
         Optional<User> user = userRepository.findByEmail(email);
-        String nickname = generateUniqueNickname();
+//        String nickname = generateUniqueNickname();
 
         if (user.isEmpty()) {
-            userRepository.save(User.toEntity(email, nickname, profileImg, passwordEncoder));
+            userRepository.save(User.toEntity(id, nickname, profileImg, passwordEncoder));
             userRepository.flush();
         }
 
