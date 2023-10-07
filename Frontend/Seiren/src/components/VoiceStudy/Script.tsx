eb8b28d970b35ea114355c46675e982871008278
정소정 <thwjd0205@naver.com>
2023-10-08 03:17:17 +0900

    Fix : VoiceRecord wav 변환 + use wavefile library

--- Frontend/Seiren/src/components/VoiceStudy/Script.tsx
+++ Frontend/Seiren/src/components/VoiceStudy/Script.tsx
@@ -1,29 +1,9 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { RecordingState, VoiceIdState, AudioDataState, RecordCountState } from '../../recoil/RecordAtom';
+import { RecordingState, AudioDataState, RecordCountState } from '../../recoil/RecordAtom';
 import { customAxios } from '../../libs/axios';
 import { useState, useEffect } from 'react';
 import styles from "./Script.module.css"
 import * as AWS from 'aws-sdk';
-import toWav from 'audiobuffer-to-wav';
-
-async function resampleAudioData(audioData, newSampleRate) {
-  // 원본 오디오 버퍼 생성
-  const audioCtx = new AudioContext();
-  const sourceBuffer = await audioCtx.decodeAudioData(audioData);
-
-  // OfflineAudioContext 객체 생성
-  const offlineCtx = new OfflineAudioContext(sourceBuffer.numberOfChannels, sourceBuffer.length * newSampleRate / sourceBuffer.sampleRate, newSampleRate);
-
-  // 소스 노드와 버퍼 설정
-  const source = offlineCtx.createBufferSource();
-  source.buffer = sourceBuffer;
-
-  // 소스 노드 연결 및 시작
-  source.connect(offlineCtx.destination);
-  source.start();
-
- return await offlineCtx.startRendering();  // 결과 데이터 반환
-}
 
 const Script: React.FC = () => {
   const [recordCount, setRecordCount] = useRecoilState(RecordCountState);
@@ -34,29 +14,42 @@ const Script: React.FC = () => {
   const audioUrl = useRecoilValue(AudioDataState);
 
   const [recordingStatus, setRecordingStatus] = useRecoilState(RecordingState);
-  const voiceId = useRecoilValue(VoiceIdState);
+  const [voiceId, setVoiceId] = useState();
+
+  useEffect(() => {
+    customAxios.get("progressingVoices")
+      .then((res) => {
+          console.log(`스크립트 페이지에서 voiceId:`, res.data.response.voiceId)
+          setVoiceId(res.data.response.voiceId);
+        })
+        .catch((error) => {
+          console.error('스크립트 페이지에서 오류:', error);
+        })
+    }, []);
+
   // 스크립트 내용
   const [nowScript, setNowScript] = useState('');
   const [nextScript, setNextScript] = useState('');
 
   // 최신 스크립트 상태 get
   useEffect(() => {
-    customAxios.get(`records/recent/${voiceId}`)
-      .then((res) => {
-        console.log('스크립트 get 요청 성공', res)
-        console.log(res.data.response)
-        setScriptId(res.data.response)
-      })
-      .catch((err) => {
-        console.error('스크립트 get 요청 에러', err)
-      });
-  }, []);
+    voiceId &&
+      customAxios.get(`records/recent/${voiceId}`)
+        .then((res) => {
+          console.log('스크립트 get 요청 성공 ID는', res.data.response)
+          setScriptId(res.data.response)
+        })
+        .catch((err) => {
+          console.error('스크립트 get 요청 에러', err)
+        });
+    }, [voiceId]);
 
   // 스크립트 get
   useEffect(()=>{
     customAxios.get(`nextScripts/${scriptId}`)
       .then((res) => {
-        console.log('다음 스크립트 get 요청 성공', res)
+        console.log('다음 스크립트 get 요청 성공 내용은', res.data.response.script)
+        console.log('다음 스크립트 get 요청 성공 ID는', res.data.response.scriptId)
         setNowScript(res.data.response.script)
         setNextScriptId(res.data.response.scriptId)
       }) 
@@ -69,7 +62,7 @@ const Script: React.FC = () => {
   useEffect(() => {
     customAxios.get(`nextScripts/${nextScriptId}`)
       .then((res) => {
-        console.log('두번째 스크립트 get 요청 성공', res)
+        console.log('두번째 스크립트 get 요청 성공 다음 스크립트 내용은', res.data.response.script)
         setNextScript(res.data.response.script)
       })
       .catch((err) => {console.error('두번째 스크립트 get요청 실패다', err)})
@@ -77,56 +70,30 @@ const Script: React.FC = () => {
 
 
   const goNext = async () => {
+    console.log(`gonext`)
     if (recordingStatus === "stopped") {
       let blob = new Blob([audioUrl], { type: "audio/wav" });
       let url = URL.createObjectURL(blob);
       console.log(url);
-
+  
       const formData = new FormData();
-
+  
       // 원본 오디오 데이터 가져옴
       let response=await fetch(url);
       let data=await response.arrayBuffer();
-
-      // 샘플링 비율을 변경한 후 Blob으로 변환함 
-      let resampled=await resampleAudioData(data,22050);
-      let wavArrayBuffer=toWav(resampled);
-      let wavBlob=new Blob([wavArrayBuffer],{type:'audio/wav'});
     
-      formData.append("file", wavBlob);
-
-      AWS.config.update({
-        region: import.meta.env.VITE_PUBLIC_REGION,
-        accessKeyId: import.meta.env.VITE_PUBLIC_ACCESSKEY,
-        secretAccessKey: import.meta.env.VITE_PUBLIC_SECRETKEY,
-      });
-      console.log(formData.get("file"));
-      const upload = new AWS.S3.ManagedUpload({
-        params: {
-          Bucket: import.meta.env.VITE_PUBLIC_BUCKET,
-          Key: "records/" + voiceId +'-'+ new Date().toISOString() + ".wav",
-          Body: formData.get("file")
-        },
-      });
-
-      const promise = upload.promise();
-
-      promise.then(
-        function (data) {
-          console.log("File uploaded successfully");
-          console.log(data.Location);
-          customAxios.post("records",{
-            "voiceId" : voiceId,
-            "scriptId" : scriptId,
-            "recordUrl":data.Location
-          }).then((res)=>{
+      let wavBlob=new Blob([data],{type:'audio/wav'});
+      
+      // Here, you can set the filename for your wav file.
+      formData.append("file", wavBlob, "filename.wav");
+  
+        customAxios.post(`records?voiceId=${voiceId}&scriptId=${nextScriptId}`, formData)
+          .then((res) => {
             console.log("녹음하고 보낸거"+res.data.response);
           })
-        },
-        function (err) {
-          return err("Audio upload failed");
-        },
-      );
+          .catch((err) => {
+            console.error('녹음한 파일 전송 실패', err)
+          })
     }
 
     customAxios.get(`records/count/${voiceId}`)
