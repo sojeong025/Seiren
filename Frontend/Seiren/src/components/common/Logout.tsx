0b3c9f4ed09542ad32f8484a62038f8f446fdb44
choiapple <79374648+choiapple@users.noreply.github.com>
2023-10-09 01:57:13 +0900

    Feat : 로그아웃 시 sse연결 끊기

--- Frontend/Seiren/src/components/common/Logout.tsx
+++ Frontend/Seiren/src/components/common/Logout.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Logout.module.css";
+import { customAxios } from "../../libs/axios";
 
 const LogoutButton: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  const handleLogout = async() => {
     // 로컬 스토리지의 모든 항목 삭제
     localStorage.clear();
+    await customAxios.delete("sse/disconnect")
+    .then((res)=>{
+      console.log(res);
+    }).catch(error => console.log(error))
 
     // 로그아웃 후 메인 페이지로 이동
     navigate("/");
