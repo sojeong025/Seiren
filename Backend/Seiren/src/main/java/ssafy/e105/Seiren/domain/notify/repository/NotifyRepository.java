374693b6c74e5d2551299dfdf7de62a1f72357d1
deerKBS <deer980119@gmail.com>
2023-10-09 03:52:31 +0900

    Feat : 알림 기능 추가

--- Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/notify/repository/NotifyRepository.java
+++ Backend/Seiren/src/main/java/ssafy/e105/Seiren/domain/notify/repository/NotifyRepository.java
@@ -2,10 +2,13 @@ package ssafy.e105.Seiren.domain.notify.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ssafy.e105.Seiren.domain.notify.entity.Notify;
-import ssafy.e105.Seiren.domain.user.entity.User;
 
 public interface NotifyRepository extends JpaRepository<Notify, Long> {
 
-    List<Notify> findByUser(User user);
+    @Query(value = "SELECT n FROM Notify n WHERE n.user.id = :userId AND n.createdAt > (NOW() - INTERVAL 1 WEEK)", nativeQuery = true)
+    List<Notify> findByUserIdAndCreatedAtAfter(@Param("userId") Long userId);
+
 }
